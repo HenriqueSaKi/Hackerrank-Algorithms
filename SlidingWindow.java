1f2dc7c638155146796657faa938ac1313e22bfc
Henrique Kisaki <satoshikisaki@hotmail.com>
2022-07-03 20:30:39 -0300

    Sliding window algorithm

--- SlidingWindow.java
+++ SlidingWindow.java
@@ -24,8 +24,10 @@ class Result {
 
     public static int birthday(List<Integer> s, int d, int m) {
         int count = 0;
+        
+        //Sliding Window Algorithm
         if(s.size() > 1){
-            for(int i = 0; i < s.size() - m; i++){
+            for(int i = 0; i <= s.size() - m; i++){
                 int sum = 0;
                 
                 for(int j = 0; j < m; j++){
